ece806ff343b07ff08c8e8e7a7d9caefbcdefcb7
Scuttie <95427459+Scuttie@users.noreply.github.com>
2024-10-01 21:34:42 +0900

    Update and rename gcd.py to gcd_lcm.py

--- Algorithms/Recursion/gcd_lcm.py
+++ Algorithms/Recursion/gcd_lcm.py
@@ -3,5 +3,9 @@ def gcd(a, b):
         return a
     return gcd(b, a % b)
 
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b)
+
 # Example usage
 print(gcd(48, 18))  # Output: 6
+print(lcm(12, 18))  # Output: 36
