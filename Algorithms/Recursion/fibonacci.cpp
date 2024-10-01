0ca490a7b6c2c0e9fe4e8bc411ca4db1fae0677c
Scuttie <95427459+Scuttie@users.noreply.github.com>
2024-10-01 21:15:28 +0900

    Create fibonacci.cpp

---
+++ Algorithms/Recursion/fibonacci.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    cout << fibonacci(5) << endl;  // Output: 5
+    return 0;
+}
